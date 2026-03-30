d4cbfffe96db5957efca06c8a65ed73cc3c13ead
Durgesh <144043419+Durgesh25052003@users.noreply.github.com>
2026-03-30 10:57:55 +0530

    Update CORS origin to production URL

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://zync-pi.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
