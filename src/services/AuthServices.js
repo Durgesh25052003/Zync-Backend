565164e12d3fec47fff91804607297c20830229d
Durgesh <144043419+Durgesh25052003@users.noreply.github.com>
2026-03-30 11:20:41 +0530

    Change SameSite cookie attribute from 'none' to 'lax'

--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -100,7 +100,7 @@ export const login = async (req, res, next) => {
 
     const cookieOptions = {
       httpOnly: true,
-      samesite: "none",
+      samesite: "lax",
       secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     };
